352d379860506137e30a7a82cd76a6e145c11069
roko-barbic <rokobarosk12345@gmail.com>
2023-06-30 14:47:57 +0200

    final

--- skripta.js
+++ skripta.js
@@ -284,7 +284,8 @@ window.createPieChart = function() {
   const width = 500;
   const height = 500;
   const radius = Math.min(width, height) / 2;
-  const labelRadius = radius + 150;
+  const labelRadius = radius + 150 +40;
+  const outerRadius = radius + 50;
   const colors = ['#ffd384', '#94ebcd', '#fbaccc', '#d3e0ea', '#fa7f72', '#23E1AE', '#198064', '#074E3B', '#07474E', '#0497A7', '#05C6DB', '#48E7F9', '#48F9E0', '#0BD0B4', '#05AC70', '#04DF91', '#4C9009', '#66C508', '#95ED3D', '#D2ED3D', '#9CB609', '#6C7E08', '#985D05', '#D48A1D', '#E9B262', '#EC5424', '#8F2605', '#986657', '#3C1B93', '#A546AB', '#8D1A55'];
 
   const svg = d3.select("#pie")
@@ -321,7 +322,7 @@ window.createPieChart = function() {
         });
     });
 
-  const labels = svg.selectAll("text")
+    const labels = svg.selectAll("text")
     .data(pie(chartData))
     .enter()
     .append("text")
@@ -329,18 +330,45 @@ window.createPieChart = function() {
       const centroid = arc.centroid(d);
       const x = centroid[0] * labelRadius / radius;
       const y = centroid[1] * labelRadius / radius;
-      return `translate(${x}, ${y})`;
+      const angle = Math.atan2(centroid[1], centroid[0]);
+      const angleDegrees = angle * (180 / Math.PI);
+      let labelRotation = angleDegrees; 
+      let labelDistance = labelRadius; 
+  
+      if (angleDegrees > 90 && angleDegrees <= 180) {
+        labelRotation += 180; 
+        labelDistance += 20; 
+      } else if (angleDegrees > -180 && angleDegrees <= -90) {
+        labelRotation -= 180; 
+      } else if (angleDegrees >=-90 && angleDegrees<=90) {
+        labelDistance -= 50; 
+      } else {
+        labelDistance -= 10; 
+      }
+      if (angleDegrees >=-90 && angleDegrees<=90) {
+        labelDistance -= 200; 
+      }
+      const xAdjusted = x * labelDistance / labelRadius;
+      const yAdjusted = y * labelDistance / labelRadius;
+  
+      return `translate(${xAdjusted}, ${yAdjusted}) rotate(${labelRotation})`; 
     })
     .attr("dy", "0.35em")
-    .attr("text-anchor", "middle")
+    .attr("text-anchor", d => {
+      const angle = Math.atan2(d.data.y, d.data.x);
+      if (angle > Math.PI / 2 || angle < -Math.PI / 2) {
+        return "end"; 
+      }
+      return "start"; 
+    })
     .text(d => d.data.name)
-    .style("opacity", 0) // Set initial opacity to 0
+    .style("opacity", 0) 
     .each(function(d, i) {
       const label = d3.select(this);
-      label.transition() // Add transition
-        .delay(i * 200) // Delay creation of each label (same as the slices)
-        .duration(500) // Set animation duration
-        .style("opacity", 1); // Fade in the labels
+      label.transition() 
+        .delay(i * 200) 
+        .duration(500) 
+        .style("opacity", 1); 
     });
     function getPercent(d){
       var country = document.getElementById("countrys").value;
@@ -384,179 +412,6 @@ window.createPieChart = function() {
 
     
 
-
-/* 
-
-window.createPieChartbyName = function(name) { 
-    var data33 = data3;
-    const svg2 = d3.select("#pie");   
-    svg2.selectAll("*").remove();
-    var year = document.getElementById("years").value;
-    document.getElementById("countrys").value = name;
-    var country = name;
-
-    var filteredData=[];
-
-    for (let i = 0; i < data33.length; i++) {
-        let item = data33[i];
-        if (item["Country/Territory"] === name && item["Year"] === parseInt(year)) {
-            
-            filteredData = Object.assign({}, item);;
-            delete filteredData["Year"];
-            break;
-        }
-    }
-        // var drowningValue = filteredData["Drowning"];
-        const chartData = Object.entries(filteredData).map(([category, value]) => ({ name: category, value }));
-
-        const width = 500;
-    const height = 500;
-    const radius = Math.min(width, height) / 2;
-    const labelRadius = radius + 150;
-    const colors = ['#ffd384','#94ebcd','#fbaccc','#d3e0ea','#fa7f72',
-    '#23E1AE','#198064','#074E3B','#07474E','#0497A7',
-    '#05C6DB','#48E7F9','#48F9E0','#0BD0B4','#05AC70',
-    '#04DF91','#4C9009','#66C508','#95ED3D','#D2ED3D',
-    '#9CB609','#6C7E08','#985D05','#D48A1D','#E9B262',
-    '#EC5424','#8F2605','#986657','#3C1B93','#A546AB',
-    '#8D1A55'];
-    // Create the SVG element
-    
-
-    const svg = d3.select("#pie")
-      .append("svg")
-      .attr("width", width)
-      .attr("height", height)
-      .append("g")
-      .attr("transform", `translate(${width / 2}, ${height / 2})`);
-
-    // Define the pie layout
-    const pie = d3.pie()
-      .value(d => d.value);
-
-    // Define the arc shape
-    const arc = d3.arc()
-      .innerRadius(0)
-      .outerRadius(radius);
-
-    // Generate the pie slices
-    const slices = svg.selectAll("path")
-      .data(pie(chartData))
-      .enter()
-      .append("path")
-      .attr("d", arc)
-      .on("mouseover", handleMouseOver)
-      .on("mouseout", handleMouseOut)
-      .attr("fill", (d, i) => colors[i]); // Use different colors for each slice
-
-    // Add labels to the slices
-    const labels = svg.selectAll("text")
-      .data(pie(chartData))
-      .enter()
-      .append("text")
-      .attr("transform", d => {
-        const centroid = arc.centroid(d);
-        const x = centroid[0] * labelRadius / radius;
-        const y = centroid[1] * labelRadius / radius;
-        return `translate(${x}, ${y})`;
-      })
-      .attr("dy", "0.35em")
-      .attr("text-anchor", "middle")
-      .text(d => d.data.name); 
-
-  //         const labelLines = svg.selectAll("line")
-  // .data(pie(chartData))
-  // .enter()
-  // .append("line")
-  // .attr("x1", d => {
-  //   const centroid = arc.centroid(d);
-  //   const midAngle = Math.atan2(centroid[1], centroid[0]);
-  //   return Math.cos(midAngle) * radius * 0.9;
-  // })
-  // .attr("y1", d => {
-  //   const centroid = arc.centroid(d);
-  //   const midAngle = Math.atan2(centroid[1], centroid[0]);
-  //   return Math.sin(midAngle) * radius * 0.9;
-  // })
-  // .attr("x2", d => {
-  //   const centroid = arc.centroid(d);
-  //   const midAngle = Math.atan2(centroid[1], centroid[0]);
-  //   return Math.cos(midAngle) * radius * 1.1;
-  // })
-  // .attr("y2", d => {
-  //   const centroid = arc.centroid(d);
-  //   const midAngle = Math.atan2(centroid[1], centroid[0]);
-  //   return Math.sin(midAngle) * radius * 1.1;
-  // })
-  // .attr("stroke", "black");
-
-  // const labelLabels = svg.selectAll("text")
-  // .data(pie(chartData))
-  // .enter()
-  // .append("text")
-  // .attr("x", d => {
-  //   const centroid = arc.centroid(d);
-  //   const midAngle = Math.atan2(centroid[1], centroid[0]);
-  //   return Math.cos(midAngle) * radius * 1.2;
-  // })
-  // .attr("y", d => {
-  //   const centroid = arc.centroid(d);
-  //   const midAngle = Math.atan2(centroid[1], centroid[0]);
-  //   return Math.sin(midAngle) * radius * 1.2;
-  // })
-  // .text(d => d.data.name)
-  // .attr("text-anchor", d => {
-  //   const centroid = arc.centroid(d);
-  //   const midAngle = Math.atan2(centroid[1], centroid[0]);
-  //   return (Math.cos(midAngle) * radius > 0) ? "start" : "end";
-  // })
-  // .attr("alignment-baseline", "middle");
-
-        
-
-    function getPercent(d){
-        var country = document.getElementById("countrys").value;
-        var year = document.getElementById("years").value;
-
-        var filteredData=[];
-        console.log(d.data.name);
-        for (let i = 0; i < data3.length; i++) {
-            let item = data3[i];
-            if (item["Country/Territory"] === country && item["Year"] === parseInt(year)) {
-                
-                filteredData = Object.assign({}, item);
-                delete filteredData["Year"];
-                delete filteredData["Country/Territory"];
-                delete filteredData["Code"];
-    
-                break;
-            }
-        }
-        console.log(country);
-        console.log(filteredData);
-        var counter =0;
-        for (let key in filteredData) {
-            
-            counter += filteredData[key];
-        }
-        console.log(counter+ " "+ d.data.value);
-        console.log((d.data.value/counter)*100);
-        return (d.data.value/counter)*100;
-    }
-
-    function handleMouseOver(d){
-        d3.select(this).attr("opacity",0.8);
-        document.getElementById("name").innerHTML = "In "+ country + " in " + year + ", " + d.data.value + " people died from " + d.data.name + ", which accounted for roughly " + getPercent(d).toFixed(4).toString() +"% of all deaths that year";
-    }
-    function handleMouseOut(d) {
-        d3.select(this).attr("opacity", 1); 
-    }
-      
-
-    
-}
-     */
-
 window.createPieChartbyName = function(name) {
   var data33 = data3;
   const svg2 = d3.select("#pie");
@@ -581,7 +436,7 @@ window.createPieChartbyName = function(name) {
   const width = 500;
   const height = 500;
   const radius = Math.min(width, height) / 2;
-  const labelRadius = radius + 150 +30;
+  const labelRadius = radius + 150 + 40;
   const colors = ['#ffd384', '#94ebcd', '#fbaccc', '#d3e0ea', '#fa7f72', '#23E1AE', '#198064', '#074E3B', '#07474E', '#0497A7', '#05C6DB', '#48E7F9', '#48F9E0', '#0BD0B4', '#05AC70', '#04DF91', '#4C9009', '#66C508', '#95ED3D', '#D2ED3D', '#9CB609', '#6C7E08', '#985D05', '#D48A1D', '#E9B262', '#EC5424', '#8F2605', '#986657', '#3C1B93', '#A546AB', '#8D1A55'];
 
   const svg = d3.select("#pie")
@@ -618,7 +473,7 @@ window.createPieChartbyName = function(name) {
         });
     });
 
-  const labels = svg.selectAll("text")
+    const labels = svg.selectAll("text")
     .data(pie(chartData))
     .enter()
     .append("text")
@@ -626,18 +481,45 @@ window.createPieChartbyName = function(name) {
       const centroid = arc.centroid(d);
       const x = centroid[0] * labelRadius / radius;
       const y = centroid[1] * labelRadius / radius;
-      return `translate(${x}, ${y})`;
+      const angle = Math.atan2(centroid[1], centroid[0]);
+      const angleDegrees = angle * (180 / Math.PI);
+      let labelRotation = angleDegrees; 
+      let labelDistance = labelRadius; 
+  
+      if (angleDegrees > 90 && angleDegrees <= 180) {
+        labelRotation += 180; 
+        labelDistance += 20; 
+      } else if (angleDegrees > -180 && angleDegrees <= -90) {
+        labelRotation -= 180; 
+      } else if (angleDegrees >=-90 && angleDegrees<=90) {
+        labelDistance -= 50; 
+      } else {
+        labelDistance -= 10; 
+      }
+      if (angleDegrees >=-90 && angleDegrees<=90) {
+        labelDistance -= 200; 
+      }
+      const xAdjusted = x * labelDistance / labelRadius;
+      const yAdjusted = y * labelDistance / labelRadius;
+  
+      return `translate(${xAdjusted}, ${yAdjusted}) rotate(${labelRotation})`; 
     })
     .attr("dy", "0.35em")
-    .attr("text-anchor", "middle")
+    .attr("text-anchor", d => {
+      const angle = Math.atan2(d.data.y, d.data.x);
+      if (angle > Math.PI / 2 || angle < -Math.PI / 2) {
+        return "end"; 
+      }
+      return "start"; 
+    })
     .text(d => d.data.name)
-    .style("opacity", 0) // Set initial opacity to 0
+    .style("opacity", 0) 
     .each(function(d, i) {
       const label = d3.select(this);
-      label.transition() // Add transition
-        .delay(i * 200) // Delay creation of each label (same as the slices)
-        .duration(500) // Set animation duration
-        .style("opacity", 1); // Fade in the labels
+      label.transition() 
+        .delay(i * 200) 
+        .duration(500) 
+        .style("opacity", 1); 
     });
 
   function getPercent(d) {
